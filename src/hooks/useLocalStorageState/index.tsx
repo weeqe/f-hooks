d5b3aff9064d859cae066043157fdd061e4f2c47
Function <1094175885@qq.com>
2022-10-24 23:34:25 +0800

    add updateState

--- src/hooks/useLocalStorageState/index.tsx
+++ src/hooks/useLocalStorageState/index.tsx
@@ -1,35 +1,55 @@
-import {useMemo, useState} from "react";
-import {isFunction} from "../utils";
+import { useMemo, useState } from "react";
+import { isFunction, isUndef } from "../utils";
 import useMemoizedFn from "../useMemoizedFn";
 
 
-interface Options {
-  defaultValue?: string | (() => string)
-  serializer?: (value: any) => string
-  deserializer?: (value: string) => any
+interface Options<T> {
+  defaultValue?: T | (() => T)
+  serializer?: (value: T) => string
+  deserializer?: (value: string) => T
 }
-
-const useLocalStorageState = (key: string, options: Options = {}) => {
-  const [state, setState] = useState(() => {
-    const value = localStorage.getItem(key)
-    const defaultValue = isFunction(options.defaultValue) ? options.defaultValue() : options.defaultValue
-    if (value === undefined) {
-      return defaultValue
+function useLocalStorageState<T>(key: string, options?: Options<T>) {
+  const [state, setState] = useState<T>(() => {
+    const raw = localStorage.getItem(key)
+    if (raw) {
+      return deserializer(raw)
     }
-    return value
+    if (isFunction(options?.defaultValue)) {
+      return options?.defaultValue()
+    }
+    return options?.defaultValue
   });
 
-  const serializerState = useMemo(() => {
-    if (isFunction(options.serializer)) {
-      return options.serializer(state)
+  const serializer = (value: T) => {
+    if (options?.serializer) {
+      return options.serializer(value)
     }
-    return JSON.stringify(state)
-  }, [state]);
+    return JSON.stringify(value)
+  }
 
-  const setValue = useMemoizedFn(() => {
+  const deserializer = (value: string) => {
+    if (options?.deserializer) {
+      return options.deserializer(value)
+    }
+    return JSON.parse(value)
+  }
 
+  const updateState = useMemoizedFn((value: T | ((prevState: T) => T)) => {
+    const currentState = isFunction(value) ? value(state) : value
+    setState(currentState)
+
+    if (isUndef(currentState)) {
+      localStorage.removeItem(key)
+    } else {
+      try {
+        localStorage.setItem(key, serializer(currentState))
+      } catch (error) {
+        console.error(error)
+      }
+    }
   })
 
+  return [state, updateState]
 }
 
 export default useLocalStorageState
--- src/hooks/utils/index.ts
+++ src/hooks/utils/index.ts
@@ -1,2 +1,3 @@
 export const isFunction  = (value: unknown): value is Function => typeof value === 'function'
 export const isString = (value: unknown): value is String => typeof value === 'string'
+export const isUndef = (value: unknown): value is undefined => typeof value === 'undefined'
