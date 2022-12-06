f9d9443bba073ef47867edc45af06a12a5f3ebaa
Function <1094175885@qq.com>
2022-12-06 23:35:20 +0800

    useCookieState

--- src/hooks/useCookie/index.tsx
+++ src/hooks/useCookie/index.tsx
@@ -1,6 +1,7 @@
 import Cookies, { CookieAttributes } from 'js-cookie'
 import { isFunction, isString } from 'lodash';
-import React, { useState } from 'react'
+import { useState } from 'react'
+import useMemoizedFn from '../useMemoizedFn';
 
 export type State = string | undefined;
 
@@ -16,9 +17,18 @@ const useCookieState = (cookieKey: string, options: Options = {}) => {
         return options.defaultValue
     })
 
-    const setCookie = (s: React.SetStateAction<State>, options?: Options) => {
-
-    }
+    const setCookie = useMemoizedFn((newValue: State | ((prevState: State) => State), newOptions?: Options) => {
+        const { defaultValue, ...restOptions } = { ...options, ...newOptions }
+        setState((prevState) => {
+            const value = isFunction(newValue) ? newValue(prevState) : newValue
+            if (value === undefined) {
+                Cookies.remove(cookieKey)
+            } else {
+                Cookies.set(cookieKey, value)
+            }
+            return value
+        })
+    })
 
     return [state, setState] as const
 }
